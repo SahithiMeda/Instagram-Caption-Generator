7dac4658d4875090900e52a574afd7f9f06d855a
divyanaimisha <31345605+divyanaimisha@users.noreply.github.com>
2020-09-26 00:14:47 +0530

    Update instaCaptionsScrapper.py

--- instaCaptionsScrapper.py
+++ instaCaptionsScrapper.py
@@ -1,26 +1,25 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[93]:
 
 
 import requests
 from bs4 import BeautifulSoup
+import xlsxwriter
+
+workbook = xlsxwriter.Workbook('instaCaptions.xlsx')
+worksheet = workbook.add_worksheet()
+
 
 my_file = open("Links.txt", "r")
 content = my_file.read()
 content_list = content.splitlines()
 my_file.close()
+row=0
 
 for i in content_list:
     page = requests.get(i)
     soup = BeautifulSoup(page.content, 'html.parser')
-    print(soup.find_all("title")[0].get_text().partition(':')[2] )
+    temp = soup.find_all("title")[0].get_text().partition(':')[2] 
+    print(temp)
+    worksheet.write(row, 0, temp)     # Writes in file instaCaptions
+    row=row+1
 
-
-
-
-    
-
-
-
+workbook.close()
