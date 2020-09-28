2af300c440cbd9477eb1f5f97ce3698b00725b7c
divyanaimisha <31345605+divyanaimisha@users.noreply.github.com>
2020-09-28 15:56:56 +0530

    excel sheet + captions scrapper updated

--- instaCaptionsScrapper.py
+++ instaCaptionsScrapper.py
@@ -3,6 +3,10 @@
 import requests
 from bs4 import BeautifulSoup
 import xlsxwriter
+import googletrans
+from googletrans import Translator
+
+
 
 workbook = xlsxwriter.Workbook('instaCaptions.xlsx')
 worksheet = workbook.add_worksheet()
@@ -14,12 +18,20 @@ content_list = content.splitlines()
 my_file.close()
 row=0
 
-for i in content_list:
-    page = requests.get(i)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    temp = soup.find_all("title")[0].get_text().partition(':')[2] 
-    print(temp)
-    worksheet.write(row, 0, temp)     # Writes in file instaCaptions
-    row=row+1
+translator = Translator()
 
+for i in content_list:
+    try:
+        page = requests.get(i)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        result = soup.find_all("title")[0].get_text().partition(':')[2] 
+        print(result)
+        result = translator.translate(result)
+        print(result.text)
+        worksheet.write(row, 0, result.text)     # Writes in file instaCaptions
+        row=row+1
+    except:
+        print("error in:"+ str(row))
+        worksheet.write(row, 0, "CAPTION NOT LOADED")     # Writes in file instaCaptions
+        row=row+1
 workbook.close()
